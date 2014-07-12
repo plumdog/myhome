2bb38c33b024155a35412bf2dec7f39ecd13f49e
Andrew Plummer <plummer574@gmail.com>
2014-07-12 16:19:12 +0100

    Default to blog page

--- myhome/myhome/urls.py
+++ myhome/myhome/urls.py
@@ -4,11 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myhome.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-                       
+    url(r'^$', include('blog.urls')), # make this cleverer
 )
