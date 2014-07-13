3b1d8cb610258a88f84f33b14d2ecdf411ebeae0
Andrew Plummer <plummer574@gmail.com>
2014-07-13 13:25:04 +0100

    Add markdown for blog

--- myhome/myhome/settings.py
+++ myhome/myhome/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'markdown_deux',
+
     'blog',
     'home'
 )
