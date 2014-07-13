bc27f65f2453c9d7f568dfaf633414bb51a3a78b
Andrew Plummer <plummer574@gmail.com>
2014-07-13 14:04:37 +0100

    Add page for single blog post

--- myhome/blog/urls.py
+++ myhome/blog/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, url
 from blog import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+urlpatterns = patterns(
+    '',
+    url(r'^post/(?P<post_id>\d+)/$', views.post, name='post'),
+    url(r'^$', views.index, name='index'))
--- myhome/blog/views.py
+++ myhome/blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import BlogPost
 
 def index(request):
-    blog_posts = BlogPost.objects.order_by('datetime')
+    blog_posts = BlogPost.objects.order_by('-datetime')
     context = {'blog_posts': blog_posts}
     return render(request, 'blog/index.djhtml', context)
+
+def post(request, post_id):
+    blog_post = get_object_or_404(BlogPost, pk=post_id)
+    context = {'bp': blog_post}
+    return render(request, 'blog/blog_post.djhtml', context)
