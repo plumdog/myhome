8d5b0682c3262fa210c3ed5e50c91259f1f2550c
Andrew Plummer <plummer574@gmail.com>
2014-07-27 09:19:17 +0100

    Set default ordering for blog post tags

--- myhome/blog/models.py
+++ myhome/blog/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class BlogPostTag(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
