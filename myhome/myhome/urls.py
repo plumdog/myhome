4b3c1dbdc0ce80897fd396b091a60ceb802f69bb
Andrew Plummer <plummer574@gmail.com>
2014-08-30 15:01:11 +0100

    More padding on blog list page

--- myhome/myhome/urls.py
+++ myhome/myhome/urls.py
@@ -1,3 +1,5 @@
+from . import settings
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -6,5 +8,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'^', include('home.urls', namespace='home'))
+    url(r'^', include('home.urls', namespace='home')),
 )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+if settings.DEBUG == True:
+    urlpatterns += staticfiles_urlpatterns()
