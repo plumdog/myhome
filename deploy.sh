f7febd795640ce3834572994af4ebe22957bfc57
Andrew Plummer <plummer574@gmail.com>
2016-02-08 22:19:08 +0000

    Don't get clever with the commit message for deployment

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR"
 
-message="$(git log -1 --pretty=%B)"
-message="$(echo -e $message)"
-
 rm -rf _html
 python main.py || error "Unable to build html pages"
 
@@ -20,6 +17,6 @@ cd _html || error "Unable to move int html directyr"
 git init
 git add .
 
-git commit -m "$message" # todo - lift the commit message from the real git repo
+git commit -m "Deploy"
 git remote add origin git@github.com:plumdog/plumdog.github.io.git
 git push -u origin master -f
