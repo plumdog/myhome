3250f799e83ea5e4f429bf1861cdbcfaabbfa901
Andrew Plummer <plummer574@gmail.com>
2016-02-08 22:16:50 +0000

    Change to use github.io

--- deploy.sh
+++ deploy.sh
@@ -17,17 +17,9 @@ python main.py || error "Unable to build html pages"
 
 cd _html || error "Unable to move int html directyr"
 
-touch composer.json
-touch index.php
-
-mv index.html home.html
-
-echo '<?php include_once("home.html"); ?>' > index.php
-echo '{}' > composer.json
-
 git init
 git add .
 
 git commit -m "$message" # todo - lift the commit message from the real git repo
-heroku git:remote -a desolate-springs-9039
-git push heroku master -f
+git remote add origin git@github.com:plumdog/plumdog.github.io.git
+git push -u origin master -f
